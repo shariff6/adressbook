30e764b30c1bf07a33471b0fcf3b868e2c2e8ed1
shariff6 <shariff.mohammed.a@gmail.com>
2017-05-09 11:14:03 +0300

    added email field

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // business logic
-function Contact(first, last, number) {
+function Contact(first, last, number, email) {
   this.firstName = first;
   this.lastName = last;
   this.phoneNumber = number;
+  this.emailAdress = email;
 }
 
 Contact.prototype.fullName = function() {
@@ -17,8 +18,9 @@ Contact.prototype.fullName = function() {
       var inputtedFirstName = $("input#new-first-name").val();
       var inputtedLastName = $("input#new-last-name").val();
       var inputtedPhoneNumber = $("input#new-phone-number").val();
+      var inputtedEmailAdress= $("input#new-email-adress").val();
 
-      var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+      var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAdress);
 
       $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -30,6 +32,7 @@ Contact.prototype.fullName = function() {
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
     $(".phone-number").text(newContact.phoneNumber);
+    $(".email-adress").text(newContact.emailAdress);
   });
 
     });
